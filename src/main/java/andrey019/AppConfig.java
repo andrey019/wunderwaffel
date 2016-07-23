25dcaf8030c76f0cab3588ff2fffbc75756bad9b
andrey019 <andrey019@gmail.com>
2016-07-23 22:24:04 +0300

    Deploy

--- src/main/java/andrey019/AppConfig.java
+++ src/main/java/andrey019/AppConfig.java
@@ -4,7 +4,10 @@ package andrey019;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -17,7 +20,7 @@ import javax.persistence.Persistence;
 @org.springframework.context.annotation.Configuration
 @ComponentScan("andrey019")
 @EnableWebMvc
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
 //    @Bean
 //    public SessionFactory sessionFactory() {
@@ -44,6 +47,16 @@ public class AppConfig {
     public EntityManager entityManager() {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPAController");
         return factory.createEntityManager();
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
     }
 
 //    @Bean
--- src/main/java/andrey019/WebConfig.java
+++ src/main/java/andrey019/WebConfig.java
@@ -1,23 +1,15 @@
-package andrey019;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-    }
-
-}
+//package andrey019;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//
+//
+//}
