e18e601db9d147569fb4caed9453e205fbdd29a0
andrey019 <andrey019@gmail.com>
2016-07-23 20:58:24 +0300

    Deploy

--- src/main/java/andrey019/MainController.java
+++ src/main/java/andrey019/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,6 +35,11 @@ public class MainController {
 		return "test_page";
 	}
 
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+        //model.addAttribute("user", getPrincipal());
+        return "admin";
+    }
 
 
 //	@RequestMapping("/favicodn.ico")
---
+++ src/main/webapp/pages/admin.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: andrey
+  Date: 23.07.2016
+  Time: 20:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>adminka</title>
+</head>
+<body>
+welcome to adminka
+</body>
+</html>
