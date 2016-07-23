327b21b6890e83b5d7ce162a8b07dfb608e36027
andrey019 <andrey019@gmail.com>
2016-07-23 17:35:46 +0300

    Deploy

---
+++ src/main/java/andrey019/TestTable.java
@@ -0,0 +1,40 @@
+package andrey019;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class TestTable {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private int number;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
