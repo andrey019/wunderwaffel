298ef90d3bb410ee1c610e34de9a7b063e17a82a
andrey019 <andrey019@gmail.com>
2016-07-25 15:08:49 +0300

    Deploy

--- src/main/java/andrey019/configuration/SecurityConfiguration.java
+++ src/main/java/andrey019/configuration/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
-        auth.authenticationProvider(authenticationProvider());
+        //auth.authenticationProvider(authenticationProvider());
     }
 
     @Bean
