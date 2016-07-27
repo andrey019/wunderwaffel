e6e20c24628854577219d689a7f21b064831b3bc
andrey019 <andrey019@gmail.com>
2016-07-27 18:50:25 +0300

    Deploy

---
+++ src/main/java/andrey019/service/RegistrationService.java
@@ -0,0 +1,12 @@
+package andrey019.service;
+
+
+import andrey019.model.User;
+
+public interface RegistrationService {
+
+    public boolean isEmailCorrect(String email);
+    public boolean isNotConfirmed(String email);
+    public boolean isEmailUnique(String email);
+    public String register(User user);
+}
