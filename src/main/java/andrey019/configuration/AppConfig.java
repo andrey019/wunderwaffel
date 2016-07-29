ffe0b1e498425596b79813f9fff9ffad032cc74a
andrey019 <andrey019@gmail.com>
2016-07-29 16:51:18 +0300

    Deploy

--- src/main/java/andrey019/configuration/AppConfig.java
+++ src/main/java/andrey019/configuration/AppConfig.java
@@ -90,19 +90,19 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return resolver;
     }
 
-    @Bean
-    public MessageSource messageSource() {
-
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasenames("classpath:messages/messages", "classpath:messages/validation");
-        // if true, the key of the message will be displayed if the key is not
-        // found, instead of throwing a NoSuchMessageException
-        messageSource.setUseCodeAsDefaultMessage(true);
-        messageSource.setDefaultEncoding("UTF-8");
-        // # -1 : never reload, 0 always reload
-        messageSource.setCacheSeconds(0);
-        return messageSource;
-    }
+//    @Bean
+//    public MessageSource messageSource() {
+//
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasenames("classpath:messages/messages", "classpath:messages/validation");
+//        // if true, the key of the message will be displayed if the key is not
+//        // found, instead of throwing a NoSuchMessageException
+//        messageSource.setUseCodeAsDefaultMessage(true);
+//        messageSource.setDefaultEncoding("UTF-8");
+//        // # -1 : never reload, 0 always reload
+//        messageSource.setCacheSeconds(0);
+//        return messageSource;
+//    }
 
 //    @Bean
 //    public CharacterEncodingFilter characterEncodingFilter() {
