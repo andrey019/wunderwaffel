b72abd8b29796135d387df627a6041694056fc76
andrey019 <andrey019@gmail.com>
2016-07-29 15:21:24 +0300

    Deploy

--- src/main/java/andrey019/dao/RegistrationDaoImpl.java
+++ src/main/java/andrey019/dao/RegistrationDaoImpl.java
@@ -16,7 +16,7 @@ public class RegistrationDaoImpl implements RegistrationDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional(noRollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public boolean save(UserConfirmation userConfirmation) {
         //entityManager.getTransaction().begin();
