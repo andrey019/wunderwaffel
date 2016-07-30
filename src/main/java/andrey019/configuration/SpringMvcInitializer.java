94aff0f49c5bd37291f9474d9bd95ad0c0bb7c4f
andrey019 <andrey019@gmail.com>
2016-07-30 14:12:25 +0300

    Deploy

--- src/main/java/andrey019/configuration/SpringMvcInitializer.java
+++ src/main/java/andrey019/configuration/SpringMvcInitializer.java
@@ -30,21 +30,7 @@ public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServ
 	}
 
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		WebApplicationContext context = getContext();
-		servletContext.addListener(new ContextLoaderListener(context));
-		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
-		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/*");
 
-	}
-
-	private AnnotationConfigWebApplicationContext getContext() {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		context.setConfigLocation("andrey019.configuration");
-		return context;
-	}
 
 
 //	@Override
