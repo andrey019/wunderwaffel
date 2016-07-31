67b04575458b08a080ef8abd8c171c09a1a1eab1
andrey019 <andrey019@gmail.com>
2016-07-31 21:15:43 +0300

    Deploy

---
+++ src/main/java/andrey019/model/PersistentLogins.java
@@ -0,0 +1,54 @@
+package andrey019.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "persistent_logins")
+public class PersistentLogins {
+
+    @Id
+    private String series;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_used", updatable = false, nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Date lastUsed;
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getLastUsed() {
+        return lastUsed;
+    }
+
+    public void setLastUsed(Date lastUsed) {
+        this.lastUsed = lastUsed;
+    }
+}
