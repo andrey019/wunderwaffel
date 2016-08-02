d7b3530817dc3679e54dc01baa17316606178a19
andrey_k19 <andrey_k19@mail.ru>
2016-08-02 15:30:12 +0300

    Deploy

--- src/main/java/andrey019/controller/UserController.java
+++ src/main/java/andrey019/controller/UserController.java
@@ -36,8 +36,8 @@ public class UserController {
 
     @RequestMapping(value = "/test", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     @ResponseBody
-    public String test(@RequestParam String name) {
-        System.out.println("!!! "+ name +" !!!");
+    public String test() {
+        System.out.println("!!!  !!!");
         return "ololo";//sdf
     }
 }
