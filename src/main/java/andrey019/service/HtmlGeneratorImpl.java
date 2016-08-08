ef7ec263685ced81547f64a08eb4fa32c6c9904b
andrey019 <andrey019@gmail.com>
2016-08-08 20:59:49 +0300

    Deploy

--- src/main/java/andrey019/service/HtmlGeneratorImpl.java
+++ src/main/java/andrey019/service/HtmlGeneratorImpl.java
@@ -11,27 +11,39 @@ import java.util.Set;
 @Service("htmlGenerator")
 public class HtmlGeneratorImpl implements HtmlGenerator {
 
-    private final static String LIST_BUTTON = "<button id=\"list=%d\" type=\"button\" class=\"list-group-item\" " +
-            "onclick=\"loadTodos(event)\" name=\"%s\" style=\"word-wrap: break-word\">" +
-            "<span id=\"badge=%d\" class=\"badge\">%d</span>%s</button>";
+//    private final static String LIST_BUTTON = "<button id=\"list=%d\" type=\"button\" class=\"list-group-item\" " +
+//            "onclick=\"loadTodos(event)\" name=\"%s\" style=\"word-wrap: break-word\">" +
+//            "<span id=\"badge=%d\" class=\"badge\">%d</span>%s</button>";
 //    private final static String DELETE_BUTTON = "<button id=\"del=%d\" type=\"button\" class=\"list-group-item\">" +
 //            "<span class=\"glyphicon glyphicon-trash\" aria-hidden=\"true\"></span></button>";
-    private final static String TODO_BUTTON = "<button id=\"todo=%d\" type=\"button\" class=\"list-group-item\" " +
-            "onclick=\"doneTodo(event)\" style=\"word-wrap: break-word\">%s" +
-            "<div style=\"font-size:11px; text-align: right\">Created by: %s.</div></button>";
-    private final static String DONE_TODO_BUTTON = "<button id=\"done=%d\" type=\"button\" " +
-            "class=\"list-group-item\" onclick=\"unDoneTodo(event)\" style=\"word-wrap: break-word; " +
-            "background-color: lightgrey\"><s>%s</s>" +
-            "<div style=\"font-size:11px; text-align: right\">Created by: %s. Done by: %s.</div></button>";
+//    private final static String TODO_BUTTON = "<button id=\"todo=%d\" type=\"button\" class=\"list-group-item\" " +
+//            "onclick=\"doneTodo(event)\" style=\"word-wrap: break-word\">%s" +
+//            "<div style=\"font-size:11px; text-align: right\">Created by: %s.</div></button>";
+//    private final static String DONE_TODO_BUTTON = "<button id=\"done=%d\" type=\"button\" " +
+//            "class=\"list-group-item\" onclick=\"unDoneTodo(event)\" style=\"word-wrap: break-word; " +
+//            "background-color: lightgrey\"><s>%s</s>" +
+//            "<div style=\"font-size:11px; text-align: right\">Created by: %s. Done by: %s.</div></button>";
 
 
     private final static String LIST_BUTTON_0 = "<button id=\"list=";
-    private final static String LIST_BUTTON_1 = "\" type=\"button\" class=\"list-group-item\" onclick=\"loadTodos(event)\" name=\"";
+    private final static String LIST_BUTTON_1 = "\" type=\"button\" class=\"list-group-item\" " +
+            "onclick=\"loadTodos(event)\" name=\"";
     private final static String LIST_BUTTON_2 = "\" style=\"word-wrap: break-word\"><span id=\"badge=";
     private final static String LIST_BUTTON_3 = "\" class=\"badge\">";
     private final static String LIST_BUTTON_4 = "</span>";
     private final static String LIST_BUTTON_5 = "</button>";
-
+    private final static String TODO_BUTTON_0 = "<button id=\"todo=";
+    private final static String TODO_BUTTON_1 = "\" type=\"button\" class=\"list-group-item\" " +
+            "onclick=\"doneTodo(event)\" style=\"word-wrap: break-word\">";
+    private final static String TODO_BUTTON_2 = "<div style=\"font-size:11px; text-align: right\">Created by: ";
+    private final static String TODO_BUTTON_3 = ".</div></button>";
+    private final static String DONE_TODO_BUTTON_0 = "<button id=\"done=";
+    private final static String DONE_TODO_BUTTON_1 = "\" type=\"button\" class=\"list-group-item\" " +
+            "onclick=\"unDoneTodo(event)\" style=\"word-wrap: break-word; background-color: lightgrey\"><s>";
+    private final static String DONE_TODO_BUTTON_2 = "</s><div style=\"font-size:11px; " +
+            "text-align: right\">Created by: ";
+    private final static String DONE_TODO_BUTTON_3 = ". Done by: ";
+    private final static String DONE_TODO_BUTTON_4 = ".</div></button>";
 
     private final static String NEW_LINE = "<br>";
     private final static int MAX_SYMBOLS_IN_LINE = 17;
@@ -40,44 +52,21 @@ public class HtmlGeneratorImpl implements HtmlGenerator {
     @Override
     public String generateTodoListsHtml(Set<TodoList> todoLists) {
         StringBuilder stringBuilder = new StringBuilder();
-        long time = System.currentTimeMillis();
         for (TodoList todoList : todoLists) {
-            stringBuilder.append(String.format(LIST_BUTTON, todoList.getId(), todoList.getName(), todoList.getId(),
-                    todoList.getTodoAmount(), addBreaks(todoList.getName())));
+            stringBuilder.append(LIST_BUTTON_0);
+            stringBuilder.append(todoList.getId());
+            stringBuilder.append(LIST_BUTTON_1);
+            stringBuilder.append(todoList.getName());
+            stringBuilder.append(LIST_BUTTON_2);
+            stringBuilder.append(todoList.getId());
+            stringBuilder.append(LIST_BUTTON_3);
+            stringBuilder.append(todoList.getTodoAmount());
+            stringBuilder.append(LIST_BUTTON_4);
+            stringBuilder.append(addBreaks(todoList.getName()));
+            stringBuilder.append(LIST_BUTTON_5);
         }
-        System.out.println(System.currentTimeMillis() - time);
-
-
-        StringBuilder stringBuilder1 = new StringBuilder();
-        time = System.currentTimeMillis();
-        for (TodoList todoList : todoLists) {
-            stringBuilder1.append(LIST_BUTTON_0);
-            stringBuilder1.append(todoList.getId());
-            stringBuilder1.append(LIST_BUTTON_1);
-            stringBuilder1.append(todoList.getName());
-            stringBuilder1.append(LIST_BUTTON_2);
-            stringBuilder1.append(todoList.getId());
-            stringBuilder1.append(LIST_BUTTON_3);
-            stringBuilder1.append(todoList.getTodoAmount());
-            stringBuilder1.append(LIST_BUTTON_4);
-            stringBuilder1.append(addBreaks(todoList.getName()));
-            stringBuilder1.append(LIST_BUTTON_5);
-        }
-        System.out.println(System.currentTimeMillis() - time);
-        return stringBuilder1.toString();
+        return stringBuilder.toString();
     }
-
-//    @Override
-//    public String generateTodoListsDeleteHtml(List<TodoList> todoLists) {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        for (TodoList todoList : todoLists) {
-//            stringBuilder.append(String.format(DELETE_BUTTON, todoList.getId()));
-//            for (int i = 1; i <= (todoList.getName().length() / MAX_SYMBOLS_IN_LINE); i++) {
-//                stringBuilder.append(NEW_LINE);
-//            }
-//        }
-//        return stringBuilder.toString();
-//    }
 
     @Override
     public String generateTodosHtml(Set<Todo> todos) {
@@ -85,15 +74,15 @@ public class HtmlGeneratorImpl implements HtmlGenerator {
             return "";
         }
         StringBuilder stringBuilder = new StringBuilder();
-//        StringBuilder testBuilder = new StringBuilder();
         for (Todo todo : todos) {
-            stringBuilder.append(String.format(TODO_BUTTON, todo.getId(), todo.getTodoText(),
-                    todo.getCreatedByName()));
-//            testBuilder.append(todo.getId());
-//            testBuilder.append(" / ");
+            stringBuilder.append(TODO_BUTTON_0);
+            stringBuilder.append(todo.getId());
+            stringBuilder.append(TODO_BUTTON_1);
+            stringBuilder.append(todo.getTodoText());
+            stringBuilder.append(TODO_BUTTON_2);
+            stringBuilder.append(todo.getCreatedByName());
+            stringBuilder.append(TODO_BUTTON_3);
         }
-//        System.out.println(testBuilder.toString());
-//        System.out.println(stringBuilder.toString());
         return stringBuilder.toString();
     }
 
@@ -104,8 +93,15 @@ public class HtmlGeneratorImpl implements HtmlGenerator {
         }
         StringBuilder stringBuilder = new StringBuilder();
         for (DoneTodo doneTodo : doneTodos) {
-            stringBuilder.append(String.format(DONE_TODO_BUTTON, doneTodo.getId(), doneTodo.getTodoText(),
-                    doneTodo.getCreatedByName(), doneTodo.getDoneByName()));
+            stringBuilder.append(DONE_TODO_BUTTON_0);
+            stringBuilder.append(doneTodo.getId());
+            stringBuilder.append(DONE_TODO_BUTTON_1);
+            stringBuilder.append(doneTodo.getTodoText());
+            stringBuilder.append(DONE_TODO_BUTTON_2);
+            stringBuilder.append(doneTodo.getCreatedByName());
+            stringBuilder.append(DONE_TODO_BUTTON_3);
+            stringBuilder.append(doneTodo.getDoneByName());
+            stringBuilder.append(DONE_TODO_BUTTON_4);
         }
         return stringBuilder.toString();
     }
