4f569eb34495ad325534da409074bd4ce4b214ab
andrey_k19 <andrey_k19@mail.ru>
2016-08-09 18:51:28 +0300

    Deploy

--- src/main/webapp/resources/js/registration.js
+++ src/main/webapp/resources/js/registration.js
@@ -50,3 +50,11 @@ function emailCheck() {
         }
     });
 }
+
+function getCSRFHeader() {
+    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    var csrfToken = $("meta[name='_csrf']").attr("content");
+    var headers = {};
+    headers[csrfHeader] = csrfToken;
+    return headers;
+}
