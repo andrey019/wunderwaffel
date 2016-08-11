6e866e815c83ec5f1d881ee15c0ba018695c26aa
andrey019 <andrey019@gmail.com>
2016-08-11 11:05:33 +0300

    Deploy

--- src/main/java/andrey019/service/auth/PasswordRecoveryImpl.java
+++ src/main/java/andrey019/service/auth/PasswordRecoveryImpl.java
@@ -7,14 +7,16 @@ import andrey019.service.MailService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("passwordRecovery")
 public class PasswordRecoveryImpl implements PasswordRecovery {
 
     private final static String MAIL_SUBJECT = "WunderWaffel password recovery";
-    private final static String MAIL_TEXT_0 = "<html><body>Your new password for WunderWaffel is <b>";
-    private final static String MAIL_TEXT_1 = "</b><br>Please, change it as soon as possible. " +
+    private final static String MAIL_TEXT_0 = "<html><body>You were trying to recover your password for " +
+            "WunderWaffel.<br><br>New password: <b>";
+    private final static String MAIL_TEXT_1 = "</b><br><br>Please, change it as soon as possible. " +
             "You can do it in your profile menu.";
     private final static String NOT_VALID = "Email is not valid!";
     private final static String NOT_FOUND = "Email is not found!";
@@ -30,6 +32,9 @@ public class PasswordRecoveryImpl implements PasswordRecovery {
     @Autowired
     private EmailValidator emailValidator;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public String generateNewPassword(String email) {
         if (!isEmailValid(email)) {
@@ -40,7 +45,7 @@ public class PasswordRecoveryImpl implements PasswordRecovery {
             return NOT_FOUND;
         }
         String password = RandomStringUtils.random(10, true, true);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         if (userDao.save(user)) {
             mailService.sendMail(email, MAIL_SUBJECT, getMailText(password));
             return OK;
