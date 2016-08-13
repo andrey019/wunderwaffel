33619faf73b3b2318b6b125cbcf4aaef208c351f
andrey019 <andrey019@gmail.com>
2016-08-13 12:07:12 +0300

    static content caching enable

--- src/main/java/andrey019/configuration/AppConfig.java
+++ src/main/java/andrey019/configuration/AppConfig.java
@@ -73,6 +73,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31536000);
     }
 }
